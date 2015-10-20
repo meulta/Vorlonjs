5fe08c740c93cca271f8d17524c0e9c4166a96ce
stephgou <stephgou@microsoft.com>
2015-10-20 23:36:48 +0200

    Launch node server

---
+++ Ops/Scripts/PreviewProd-Provision.ps1
@@ -0,0 +1,57 @@
+﻿#https://github.com/meulta/Vorlonjs/blob/development-0.0.16/azuredeploy.json
+
+Param(  
+    #Paramètres du Azure Ressource Group
+    $resourceGroupeName = "ProdVorlonjs",
+    $resourceGroupeDeploymentName = "ProdVorlonjsDeployed",
+    $resourceLocation = "West Us",
+    $publishSettingsFile = (Resolve-Path "C:\DEMOS\21 - CLOUD\AZURE\Azure Keys\stephgou - External.publishsettings"),
+    $subscriptionName = "stephgou - External",
+    $templateFile = "azuredeploy.json",
+    $templateParameterFile = "prod-azuredeploy-parameters.json",
+    $templateFolder = "..\templates",
+    $tagName = "Vorlonjs_RG",
+    $tagValue = "Prod"
+    )
+
+#region init
+Set-PSDebug -Strict
+
+cls
+$d = get-date
+Write-Host "Starting Deployment $d"
+
+$scriptFolder = Split-Path -Parent $MyInvocation.MyCommand.Definition
+Write-Host "scriptFolder" $scriptFolder
+
+set-location $scriptFolder
+#endregion init
+
+#region Souscription
+#Remove-AzureAccount 
+#Add-AzureAccount
+#Get-AzureAccount
+Select-AzureSubscription -SubscriptionName $subscriptionName
+#endregion Souscription
+
+Switch-AzureMode -Name AzureResourceManager
+
+# Resource groupe create
+New-AzureResourceGroup `
+	-Name $resourceGroupeName `
+	-Location $resourceLocation `
+    -Tag @{Name=$tagName;Value=$tagValue} `
+    -Verbose
+
+# Resource group deploy
+New-AzureResourceGroupDeployment `
+    -Name $resourceGroupeDeploymentName `
+	-ResourceGroupName $resourceGroupeName `
+	-TemplateFile "$scriptFolder\$templatefolder\$templateFile" `
+	-TemplateParameterFile "$scriptFolder\$templatefolder\$templateParameterFile" `
+    -Verbose `
+    #-StorageAccountName $storageAccountName `
+
+
+$d = get-date
+Write-Host "Stopping Deployment $d"
