df6c9d1e4bfd8d47137b855afddd9e8b03910576
Etienne Margraff <etienne.margraff@live.fr>
2015-10-20 10:25:58 -0700

    Test

--- Server/test/test.js
+++ Server/test/test.js
@@ -1,7 +1,17 @@
 var assert = require('assert');
 var vorlontools = require("../Scripts/vorlon.tools");
 
-('Array', function() {
+describe('Array', function() {
+  describe('#indexOf()', function () {
+    it('should return -1 when the value is not present', function () {
+      assert.equal(-1, [1,2,3].indexOf(5));
+      assert.equal(-1, [1,2,3].indexOf(0));
+    });
+  });
+});
+
+
+describe('Array', function() {
   describe('toolsUserAgent', function () {
     it('should return Android for android user agent', function () {
       var ua = "Mozilla/5.0 (Linux; <Android Version>; <Build Tag etc.>) AppleWebKit/<WebKit Rev> (KHTML, like Gecko) Chrome/<Chrome Rev> Mobile Safari/<WebKit Rev> ";
